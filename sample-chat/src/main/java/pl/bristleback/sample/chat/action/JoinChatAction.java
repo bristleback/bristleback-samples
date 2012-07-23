6e21d73413e14b750594bc55dfb2b0ec28b6f702
wojniemiec <woj.niemiec@gmail.com>
2012-07-23 21:07:35 +0200

    chat sample - fixes in validation

--- sample-chat/src/main/java/pl/bristleback/sample/chat/action/JoinChatAction.java
+++ sample-chat/src/main/java/pl/bristleback/sample/chat/action/JoinChatAction.java
@@ -2,12 +2,13 @@ package pl.bristleback.sample.chat.action;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import pl.bristleback.server.bristle.api.action.DefaultAction;
-import pl.bristleback.server.bristle.api.annotations.Action;
-import pl.bristleback.server.bristle.api.annotations.AnnotatedActionClass;
 import pl.bristleback.sample.chat.action.client.ChatClientAction;
 import pl.bristleback.sample.chat.user.ActiveUsers;
 import pl.bristleback.sample.chat.user.ChatUser;
+import pl.bristleback.server.bristle.api.action.DefaultAction;
+import pl.bristleback.server.bristle.api.annotations.Action;
+import pl.bristleback.server.bristle.api.annotations.AnnotatedActionClass;
+import pl.bristleback.server.bristle.api.annotations.Bind;
 
 import java.util.List;
 
@@ -22,7 +23,7 @@ public class JoinChatAction implements DefaultAction<ChatUser, String> {
   private ChatClientAction chatClientAction;
 
   @Action
-  public List<ChatUser> executeDefault(ChatUser user, String userName) {
+  public List<ChatUser> executeDefault(ChatUser user, @Bind(required = true) String userName) {
     activeUsers.registerUser(userName, user);
 
     chatClientAction.newUser(userName, activeUsers.getUsers());
