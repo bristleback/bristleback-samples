636f6c97847814685354b574dcf094c0be1b8e6f
machowski <paw.machowski@gmail.com>
2013-01-27 15:05:01 +0100

    simplifying tutorial sample

--- tutorial-chat/src/main/webapp/js/bristleChat.js
+++ tutorial-chat/src/main/webapp/js/bristleChat.js
@@ -1,29 +1,29 @@
-    /* main container for our chat application */
-    var Chat = {};
+/* main container for our chat application */
+var Chat = {};
 
-    function prepareClient() {
-      var config = {
-        /* url of backend server, 8765 is default port for standalone app, can be changed in server configuration */
-        serverUrl: "ws://localhost:8765/websocket",
-        /* function invoked after establishing connection */
-        OnOpen: function (event) {
-          /**
-           *   Bristleback.CONNECTOR - alias for current user used in java script client
-           *   Chat.username - variable will hold name of logged user
-           */
-          Chat.joinChatActionClass.executeDefault(Bristleback.CONNECTOR, Chat.username);
-        },
-        /* function invoked when connection is closed*/
-        OnClose: function (event) {
-          alert("disconnected");
-          Chat.joinChatActionClass.executeDefault(Bristleback.CONNECTOR, Chat.username);
-        }
-      };
-      /*new bristleback client created*/
-      Chat.client = Bristleback.newClient(config);
-      /* for simplicity we assign data bristleback data controller to our chat object*/
-      Chat.dataController = Chat.client.dataController;
+function prepareClient() {
+  var config = {
+    /* url of backend server, 8765 is default port for standalone app, can be changed in server configuration */
+    serverUrl: "ws://localhost:8765/websocket",
+    /* function invoked after establishing connection */
+    OnOpen: function (event) {
+      /**
+       *   Bristleback.CONNECTOR - alias for current user used in java script client
+       *   Chat.username - variable will hold name of logged user
+       */
+      Chat.joinChatActionClass.executeDefault(Bristleback.CONNECTOR, Chat.username);
+    },
+    /* function invoked when connection is closed*/
+    OnClose: function (event) {
+      alert("disconnected");
+      Chat.joinChatActionClass.executeDefault(Bristleback.CONNECTOR, Chat.username);
     }
+  };
+  /*new bristleback client created*/
+  Chat.client = Bristleback.newClient(config);
+  /* for simplicity we assign data bristleback data controller to our chat object*/
+  Chat.dataController = Chat.client.dataController;
+}
 
 
 /*----------------- ACTION CLASSES*/
@@ -34,6 +34,11 @@ function prepareActionClasses() {
 }
 
 function defineJoinChatActionClass() {
+  /**
+   * we're defining new action class - JoinChat - same name needs to be used in Java action class
+   * we also define response handler for default action - onLogInCallback function
+   * and two handlers fro exceptions
+   */
   Chat.joinChatActionClass = Chat.dataController.getActionClass("JoinChat");
   Chat.joinChatActionClass.defineDefaultAction().setResponseHandler(onLogInCallback)
     .exceptionHandler
@@ -41,25 +46,28 @@ function defineJoinChatActionClass() {
     .setExceptionHandler("UserExistsException", userExistsErrorCallback);
 
   function onLogInCallback(users) {
-    switchToLoggedScreen();
-
-    var messages = $("#messages");
-    scrollDownChat();
-    $("#speakChannel").focus();
-    actualUsersList(users)
+    switchToLoggedScreen(); //show messages + users screeen
+    scrollDownChat();  //to display most recent message
+    $("#speakChannel").focus(); //to position cursor in input area
+    actualUsersList(users); //create list with logged users
   }
 
+  //callback for exceptionHandler
   function validationErrorCallback() {
-    switchToLoginScreen();
-    $("#loginErrors").html("Username required");
+    alert("Username required");
   }
 
+  //callback for exceptionHandler
   function userExistsErrorCallback() {
-    switchToLoginScreen();
-    $("#loginErrors").html("User already exists, choose other nickname");
+    alert("User already exists, choose other nickname");
   }
 }
 
+/**
+ * we're defining new action class - SendText - same name needs to be used in Java action class
+ * This time we're only initializing action class by creating default action (executeDefault method in Java action class)
+ * Action will be used only for sending message to server with no response, so we don't need to specify response handler
+ */
 function defineSendTextActionClass() {
   Chat.sendTextActionClass = Chat.dataController.getActionClass("SendText");
   Chat.sendTextActionClass.defineDefaultAction();
@@ -68,34 +76,29 @@ function defineSendTextActionClass() {
 /*----------------- CLIENT ACTION CLASSES*/
 
 function prepareClientActionClass() {
+  /**
+   * Each method of below object will handle different client action (event from server)
+   * names of method needs to be same as method names in Java client
+   */
   var chatClientActionClass = {
+    //new user has joined the chat
     newUser: function (userName, actualList) {
-      addMessage(userName + " has joined chat");
-      actualUsersList(actualList);
+      addMessage(userName + " has joined chat"); //we're displaying info message with username
+      actualUsersList(actualList); //we need to update user list with new content (as argument we've go list of all users)
     },
 
+    //new message was sent, we need to add it to messages list
     sendText: function (user, chatText) {
-      var userOnList = $("#" + user.id);
-      if (chatText.finished) {
-        addMessage(Bristleback.utils.escapeHTML(user.nickname) + ":", Bristleback.utils.escapeHTML(chatText.text));
-        userOnList.empty();
-      } else {
-        userOnList.empty();
-        if (chatText.text !== "") {
-          userOnList.append("<table><tr><td><img src=\"img/ajax-loader.gif\"></td><td>" +
-            Bristleback.utils.escapeHTML(chatText.text) + "</td></tr></table>");
-        }
-      }
+      addMessage(Bristleback.utils.escapeHTML(user.nickname) + ":", Bristleback.utils.escapeHTML(chatText.text));
     },
 
     userLeftChat: function (userName, actualList) {
-      addMessage(userName + " has left chat");
-      actualUsersList(actualList);
+      addMessage(userName + " has left chat"); //we're displaying info message with username
+      actualUsersList(actualList); //we need to update user list with new content (as argument we've go list of all users)
     }
   };
-
+  // new client action needs to be registered in action controller
   Chat.dataController.registerClientActionClass("ChatClientAction", chatClientActionClass);
-
 }
 
 /* ---------------- GUI*/
@@ -103,37 +106,14 @@ function prepareClientActionClass() {
 function addGUIListeners() {
   $("#loginButton").click(function () {
     Chat.username = $("#login").val();
-    if (Chat.client.isConnected()) {
-      switchToLoggingScreen();
-      Chat.joinChatActionClass.executeDefault(Bristleback.CONNECTOR, Chat.username);
-    } else {
-      Chat.client.connect();
-      switchToConnectingScreen();
-    }
+    Chat.client.connect();
   });
-
-  $("#speakChannel").keyup(function (e) {
-    if (e.keyCode == 13) {
-      sendText(true);
-    } else {
-      sendText(false);
-    }
-  });
-
-  function sendText(finished) {
-    var textInput = $("#speakChannel");
-    var textMessage = {
-      text: textInput.val(),
-      finished: finished
-    };
-    Chat.sendTextActionClass.executeDefault(Bristleback.CONNECTOR, textMessage);
-    if (finished) {
-      textInput.val("");
-    }
-  }
 }
 
 function addMessage(boldText, normalText) {
+  // ?
+  Chat.sendTextActionClass.executeDefault(Bristleback.CONNECTOR, boldText);
+
   normalText = normalText || "";
   $("#messages").append("<li><span>" + boldText + " </span>" + normalText + "</li>");
   scrollDownChat();
@@ -154,31 +134,12 @@ function scrollDownChat() {
   chatFrame.scrollTop(chatFrame[0].scrollHeight);
 }
 
-function switchToLoginScreen() {
-  $('#afterLogInPane').hide();
-  $('#loggingPage').hide();
-  $('#loginPage').show();
-  setStatus("disconnected", "status-busy");
-}
-
-function switchToConnectingScreen() {
-  $('#loginPage').hide();
-  $('#afterLogInPane').hide();
-  $('#loggingPage').show();
-  setStatus("connecting", "status-away")
-}
 
 function switchToLoggedScreen() {
   $("#messages").text("");
   $('#loginPage').hide();
   $('#loggingPage').hide();
   $('#afterLogInPane').show();
-  setStatus("logged in", "status");
-}
-
-function setStatus(statusDescription, iconName) {
-  $('#connectionStatus').text(statusDescription);
-  $('#statusIcon').attr("src", "img/" + iconName + ".png");
 }
 
 /* ---------------- WIRING ALL TOGETHER*/
