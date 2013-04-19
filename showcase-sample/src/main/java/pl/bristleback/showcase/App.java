5576b95eca9b5e0f85b69c32b5d81cf38390e7ec
wojniemiec <woj.niemiec@gmail.com>
2013-04-19 20:47:20 +0200

    showcase development started

---
+++ showcase-sample/src/main/java/pl/bristleback/showcase/App.java
@@ -0,0 +1,39 @@
+package pl.bristleback.showcase;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pl.bristleback.server.bristle.app.StandaloneServerRunner;
+
+import java.util.Scanner;
+
+public class App {
+
+  private static final String STOP_CHAR = "x";
+
+  private static final String[] CONFIG_FILES =
+    {"applicationContext.xml"};
+
+  private App() {
+    throw new UnsupportedOperationException();
+  }
+
+  public static void main(String[] args) {
+    BasicConfigurator.configure();
+    Logger.getLogger("org.apache").setLevel(Level.INFO);
+
+    ApplicationContext applicationContext = new ClassPathXmlApplicationContext(CONFIG_FILES);
+
+    StandaloneServerRunner runner = (StandaloneServerRunner) applicationContext.getBean("bristlebackStandaloneServer");
+
+    Scanner in = new Scanner(System.in);
+    String value = in.nextLine();
+    while (!value.equalsIgnoreCase(STOP_CHAR)) {
+      value = in.nextLine();
+    }
+    runner.stopServer();
+  }
+}
+
