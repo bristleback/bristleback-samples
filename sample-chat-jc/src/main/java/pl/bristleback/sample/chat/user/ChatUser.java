b2a03e22c205046065ff3fe6642518341cb088d7
wojniemiec <woj.niemiec@gmail.com>
2013-05-20 22:21:37 +0200

    changes according to changes in bristleback framework

--- sample-chat-jc/src/main/java/pl/bristleback/sample/chat/user/ChatUser.java
+++ sample-chat-jc/src/main/java/pl/bristleback/sample/chat/user/ChatUser.java
@@ -1,26 +1,10 @@
 package pl.bristleback.sample.chat.user;
 
-import pl.bristleback.server.bristle.api.users.IdentifiedUser;
+import pl.bristleback.server.bristle.engine.user.BaseUserContext;
 
-import java.util.UUID;
+public class ChatUser extends BaseUserContext {
 
-public class ChatUser implements IdentifiedUser {
-
-  private String id;
   private String nickname;
-
-  public ChatUser() {
-    this.id = UUID.randomUUID().toString();
-  }
-
-  @Override
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
 
   public String getNickname() {
     return nickname;
--- sample-chat-jc/src/main/java/pl/bristleback/sample/chat/web/config/BristlebackServletConfig.java
+++ sample-chat-jc/src/main/java/pl/bristleback/sample/chat/web/config/BristlebackServletConfig.java
@@ -1,19 +1,21 @@
 package pl.bristleback.sample.chat.web.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
-
+import pl.bristleback.sample.chat.user.ChatUser;
+import pl.bristleback.server.bristle.api.users.UserContext;
 import pl.bristleback.server.bristle.conf.resolver.init.PojoConfigResolver;
 import pl.bristleback.server.bristle.engine.servlet.BristlebackHttpHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Configuration class for annotation application context.
+ *
  * @author Tom-Steve Watzke
  * @see pl.bristleback.server.bristle.conf.namespace.BristlebackServletBeanDefinitionParser
  */
@@ -21,53 +23,53 @@ import pl.bristleback.server.bristle.engine.servlet.BristlebackHttpHandler;
 @Profile("servlet")
 @Import(BristlebackServerMessagesConfig.class)
 public class BristlebackServletConfig {
-	
-	/**
-	 * Servlet engine to be used.
-	 */
-	private final static String ENGINE_NAME = "system.engine.tomcat.servlet";
-	
-	/**
-	 * Logging level for debugging using Log4J. 
-	 */
-	private final static String LOGGING_LEVEL = "org.apache.log4j.Level.DEBUG";
-	
-	/**
-	 * User factory bean name string.
-	 */
-	private final static String USER_FACTORY = "chatUserFactory";
-	
-	/**
-	 * Default: "**\/*" (without "\")
-	 */
-	private final static String SERVLET_MAPPING = "/websocket"; 
-		
-	@Bean
-	public PojoConfigResolver initialConfigurationResolver() {
-		PojoConfigResolver configResolver = new PojoConfigResolver();
-		configResolver.setEngineName(ENGINE_NAME);
-		configResolver.setLoggingLevel(LOGGING_LEVEL);
-		configResolver.setUserFactory(USER_FACTORY);
-		
-		return configResolver;
-	}
-	
-	@Bean
-	public SimpleUrlHandlerMapping bristlebackHandlerMappings() {
-		Map<String, BristlebackHttpHandler> mappings = new HashMap<>();
-		mappings.put(SERVLET_MAPPING, bristlebackHttpHandler());
-		
-		SimpleUrlHandlerMapping handlerMapping = new SimpleUrlHandlerMapping();
-		handlerMapping.setUrlMap(mappings);
-		
-		return handlerMapping;
-	}
 
-	@Bean
-	public BristlebackHttpHandler bristlebackHttpHandler() {
-		BristlebackHttpHandler httpHandler = new BristlebackHttpHandler();
-		httpHandler.setInitialConfigurationResolver(initialConfigurationResolver());
-		
-		return httpHandler;
-	}
+  /**
+   * Servlet engine to be used.
+   */
+  private final static String ENGINE_NAME = "system.engine.tomcat.servlet";
+
+  /**
+   * Logging level for debugging using Log4J.
+   */
+  private final static String LOGGING_LEVEL = "org.apache.log4j.Level.DEBUG";
+
+  /**
+   * User context class
+   */
+  private final static Class<? extends UserContext> USER_CONTEXT = ChatUser.class;
+
+  /**
+   * Default: "**\/*" (without "\")
+   */
+  private final static String SERVLET_MAPPING = "/websocket";
+
+  @Bean
+  public PojoConfigResolver initialConfigurationResolver() {
+    PojoConfigResolver configResolver = new PojoConfigResolver();
+    configResolver.setEngineName(ENGINE_NAME);
+    configResolver.setLoggingLevel(LOGGING_LEVEL);
+    configResolver.setUserContextClass(USER_CONTEXT);
+
+    return configResolver;
+  }
+
+  @Bean
+  public SimpleUrlHandlerMapping bristlebackHandlerMappings() {
+    Map<String, BristlebackHttpHandler> mappings = new HashMap<>();
+    mappings.put(SERVLET_MAPPING, bristlebackHttpHandler());
+
+    SimpleUrlHandlerMapping handlerMapping = new SimpleUrlHandlerMapping();
+    handlerMapping.setUrlMap(mappings);
+
+    return handlerMapping;
+  }
+
+  @Bean
+  public BristlebackHttpHandler bristlebackHttpHandler() {
+    BristlebackHttpHandler httpHandler = new BristlebackHttpHandler();
+    httpHandler.setInitialConfigurationResolver(initialConfigurationResolver());
+
+    return httpHandler;
+  }
 }
